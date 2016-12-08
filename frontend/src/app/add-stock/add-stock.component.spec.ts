fef5c30103528f4a9290fa7d941a0550221f263d
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2016-12-08 22:28:27 +0100

    add-stocks component tests

--- frontend/src/app/add-stock/add-stock.component.spec.ts
+++ frontend/src/app/add-stock/add-stock.component.spec.ts
@@ -1,28 +1,57 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Rx';
+import { Stock } from './../models/stock';
+import { HttpModule } from '@angular/http';
+import { StockService } from './../services/stock.service';
+import { Logger } from 'angular2-logger/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddStockComponent } from './add-stock.component';
+
+class MockLogger {
+  debug(str) { }
+}
+
+class MockStockService {
+  public addStock(body: Object): Observable<Stock> {
+    let fakeStock: Stock = new Stock(1, 'US0378331005', 'AAPL', 'Apple', new Date('2016-11-25T21:50:05.000+0100'));
+    return Observable.of(fakeStock);
+  }
+}
 
 describe('AddStockComponent', () => {
   let component: AddStockComponent;
-  let fixture: ComponentFixture<AddStockComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddStockComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddStockComponent);
+    TestBed.configureTestingModule({
+      declarations: [AddStockComponent],
+      providers: [
+        { provide: Logger, useClass: MockLogger },
+      ],
+      imports: [HttpModule, FormsModule, ReactiveFormsModule]
+    });
+
+    TestBed.overrideComponent(AddStockComponent, {
+      set: {
+        providers: [{ provide: StockService, useClass: MockStockService }]
+      }
+    });
+
+    const fixture: ComponentFixture<AddStockComponent> = TestBed.createComponent(AddStockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  /*it('should create', () => {
-    expect(component).toBeTruthy();
-  });*/
+  it('should have a defined component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should emit event when stock is added', () => {
+    let expectedEventStock = new Stock(1, 'US0378331005', 'AAPL', 'Apple', new Date('2016-11-25T21:50:05.000+0100'));
+    spyOn(component.onStockAdded, 'emit');
+
+    component.addStock();
+    expect(component.onStockAdded.emit).toHaveBeenCalledWith(expectedEventStock);
+  });
+
 });
