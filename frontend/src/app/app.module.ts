8c2a954117d5cb37c92870d62bc92cb039dacba6
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2016-12-09 21:41:14 +0100

    init routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/core';
@@ -7,18 +8,32 @@ import { LOG_LOGGER_PROVIDERS } from 'angular2-logger/core';
 import { AppComponent } from './app.component';
 import { StocksComponent } from './stocks/stocks.component';
 import { AddStockComponent } from './add-stock/add-stock.component';
+import { StockDetailsComponent } from './stock-details/stock-details.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { StockPageComponent } from './stock-page/stock-page.component';
+
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'stock', component: StockPageComponent }
+];
+
+export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
     declarations: [
         AppComponent,
         StocksComponent,
-        AddStockComponent
+        AddStockComponent,
+        StockDetailsComponent,
+        HomePageComponent,
+        StockPageComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        routing
     ],
     providers: [LOG_LOGGER_PROVIDERS],
     bootstrap: [AppComponent]
